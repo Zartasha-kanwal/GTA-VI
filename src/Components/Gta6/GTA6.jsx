432802ee5af0ff6423c0dac5995439de4a7d9663
Zartasha Kanwal <zartashakanwal529@gmail.com>
2025-07-18 12:47:03 +0500

    Update GSAP animation with 2xl screen support

--- src/Components/Gta6/GTA6.jsx
+++ src/Components/Gta6/GTA6.jsx
@@ -77,6 +77,7 @@ export default function GTA6() {
       scale: 1.5,
       top: isLargeScreen ? "34%" : "40%",
       x: "-40%",
+      y:"-40%",
       rotate: 0,
       duration: 2,
       delay: -0.8,
@@ -179,7 +180,7 @@ export default function GTA6() {
 
               <img
                 src={girl}
-                className="girl absolute left-1/2  scale-[2] rotate-[-10deg] transform -translate-x-1/2 -translate-y-1/2 max-h-[50%] lg:h-[100%]   "
+                className="girl absolute left-1/2  scale-[2] rotate-[-10deg] transform -translate-x-1/2 -translate-y-1/2 max-h-[100%]   "
               />
             </div>
             <div className="bottombar text-white absolute bottom-0 left-0 w-full py-10 px-10 bg-gradient-to-t  from-black to-transparent">
